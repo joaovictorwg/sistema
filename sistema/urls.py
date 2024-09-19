127a8b0c1ca080f54f7facd10427acf3cb895b43
João Victor Walcacer Giani <joaovictorgiani@gmail.com>
2024-09-18 21:15:27 -0300

    creat authentication

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from sistema.views import Login
+from sistema.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Login.as_view(), name='Login')
+    path('', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('veiculos/', Veiculos.as_view(), name='veiculos')
 ]
 
--- sistema/views.py
+++ sistema/views.py
@@ -1,11 +1,41 @@
 from django.http import HttpResponse
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
 
 class Login(View):
 
     def get(self, request):
+        contexto = {'mensagem': ''}
+        if request.user.is_authenticated:
+            return redirect('/veiculos')
+        else:
+            return render(request, 'autenticacao.html', contexto)
 
-        contexto =  {'mensagem': ''}
+    def post(self, request):
+        # Obtem as credenciais de autenticação do form
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
 
-        return render(request, 'autenticacao.html', contexto)
+        user = authenticate(request, username=usuario, password=senha)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("/veiculos")
+            return render(request, 'autenticacao.html', {'mensagem': 'Usuário inativo'})
+        
+        return render(request, 'autenticacao.html', {'mensagem': 'Usuário ou senha incorretos'})
+
+class Logout(View):
+    
+    def get(self, request):
+        logout(request)
+        return redirect(settings.LOGIN_URL)
+    
+class Veiculos(View):
+    
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, 'veiculos.html')  # Certifique-se de que este template existe
+        return redirect('login')  # Redireciona para a página de login se não estiver autenticado
